d98ca8a488011366581009bcf18dd6cf9db55b22
chisu-kawahara <chisu.kawahara@gmail.com>
2025-06-28 14:09:42 -0700

    Add query to get total student assist requests

--- 4_queries/2_total_student_assistance_requests.sql
+++ 4_queries/2_total_student_assistance_requests.sql
@@ -1,5 +1,5 @@
-SELECT COUNT(*) AS total_assistances, students.name
+SELECT count(assistance_requests.*) as total_assistances, students.name
 FROM assistance_requests
 JOIN students ON students.id = student_id
-WHERE students.name = 'Elliot Dickinson'
+WHERE name = 'Elliot Dickinson'
 GROUP BY students.name;
