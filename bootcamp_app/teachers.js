ff4a06da569135ae5fcf2a1e845ce1ed74e7c4bd
chisu-kawahara <chisu.kawahara@gmail.com>
2025-07-08 00:13:56 -0700

    add new queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -11,21 +11,22 @@ const pool = new Pool({
 const cohortName = process.argv[2] || "JUL02";
 const values = [cohortName];
 
-const query = `
-  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-  FROM teachers
-  JOIN assistance_requests ON teacher_id = teachers.id
-  JOIN students ON student_id = students.id
-  JOIN cohorts ON cohort_id = cohorts.id
-  WHERE cohorts.name = $1
-  ORDER BY teacher;
-`;
-
 pool
-  .query(query, values)
-  .then(res => {
-    res.rows.forEach(row => {
+  .query(
+    `
+SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = $1
+ORDER BY teacher;
+`,
+    values
+  )
+  .then((res) => {
+    res.rows.forEach((row) => {
       console.log(`${row.cohort}: ${row.teacher}`);
     });
   })
-  .catch(err => console.error("query error", err.stack));
+  .catch((err) => console.error("query error", err.stack));
