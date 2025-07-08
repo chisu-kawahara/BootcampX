fc93a9ae08d29daf4942144737d5a5f179e2bbc1
chisu-kawahara <chisu.kawahara@gmail.com>
2025-07-08 00:06:15 -0700

    add queries in teachers.js using parameterized query syntax

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,16 @@ const pool = new Pool({
   database: "bootcampx"
 });
 
-const cohortName = process.argv[2];
+// Get cohort name
+const cohortName = process.argv[2] || "JUL02";
 const values = [cohortName];
 
 const query = `
-  SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
-  FROM assistance_requests
-  JOIN teachers ON teachers.id = teacher_id
-  JOIN students ON students.id = student_id
-  JOIN cohorts ON cohorts.id = cohort_id
+  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+  FROM teachers
+  JOIN assistance_requests ON teacher_id = teachers.id
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id
   WHERE cohorts.name = $1
   ORDER BY teacher;
 `;
