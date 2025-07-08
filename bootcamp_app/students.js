96a352246e033d453f62f7b4bd60dfff83cbd29b
chisu-kawahara <chisu.kawahara@gmail.com>
2025-07-08 00:21:07 -0700

    use parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -4,24 +4,24 @@ const pool = new Pool({
   user: "development",
   password: "development",
   host: "localhost",
-  database: "bootcampx"
+  database: "bootcampx",
 });
 
 const cohortName = process.argv[2];
 const limit = process.argv[3] || 5;
+
 const values = [`%${cohortName}%`, limit];
 
+const query = `
+  SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+`;
+
 pool
-  .query(
-    `
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE $1
-LIMIT 5;
-`,
-    values
-  )
+  .query(query, values)
   .then((res) => {
     res.rows.forEach((user) => {
       console.log(
